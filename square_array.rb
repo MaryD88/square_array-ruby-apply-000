64f500b9a5ed5c874d89dd4fe5b62005701e1ce9
proactive-researcher-8324 <marymichelle1118@gmail.com>
2020-09-27 01:00:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-  puts array.each do | square_array |
-  new_array = [square_array ** 2]
+  puts array.each do | integer |
+  new_array = [integer ** 2]
   return new_array
   end
     
